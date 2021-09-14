44f5108b13a9d38e509622548c21ea83b3b78ebc
Mostafa772 <47507394+Mostafa772@users.noreply.github.com>
2021-09-14 20:54:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,83 +2,146 @@
 #include <cmath>
 #include <cstdlib>
 #include <cctype>
-
-void find_quadratic_roots(int a, int b, int c, double *x1, double *x2) {
-
-    int Discriminant = (b * b) - 4 * a * c;
-
-    if (Discriminant == 0) {
-
-        printf("There is a double root for the quadratic equation\n");
-        *x2 = 0;
-        *x1 = -c * 1.0 / b;
-        printf("the root x: %lf\n", *x1);
-
-    } else if (Discriminant > 0) {
-
-        printf("There are two real roots\n");
-        *x1 = (-b - sqrt(Discriminant)) / 2 * a;
-        *x2 = (-b + sqrt(Discriminant)) / 2 * a;
-
-        printf("the first root x1: %lf\nthe second root x2: %lf", *x1, *x2);
+#include <assert.h>
 
 
-    } else if (Discriminant < 0) {
+const double epsilon = 1e-10;
 
-        printf("There are two imaginary roots\n");
-        double x1_real_part = -b / 2.0 * a;
-        double x1_imaginary_part = sqrt(abs(Discriminant)) / 2.0 * a;
-        printf("x1 = %lf + i %lf\n", x1_real_part, x1_imaginary_part);
+bool If_Double_Equal_Zero (double a) {
+    return (fabs(a) <= epsilon);
+}
 
-        double x2_real_part = -b / 2.0 * a;
-        double x2_imaginary_part = sqrt(abs(Discriminant)) / 2.0 * a;
-        printf("x2 = %lf - i %lf\n", x2_real_part, x2_imaginary_part);
+enum SolutionType {
+    REAL_ROOTS_WITH_DIFFERENT_VALUES = 1,
+    REAL_ROOTS_WITH_SAME_VALUES,
+    IMAGINARY_ROOTS,
+    LINEAR_EQUATION,
+    NO_ROOTS,
+    INFINITE_NUMBER_OF_ROOTS
+};
 
+struct Solution {
+    enum SolutionType type;
+    double first_root;
+    double second_root;
+    double real_part;
+    double imaginary_part;
+    double one_root;
+};
+
+enum SolutionType find_root_type(double discriminant, double a, double b, double c) {
+
+    if (discriminant > 0) {
+
+        return REAL_ROOTS_WITH_DIFFERENT_VALUES;
+
+    } else if (If_Double_Equal_Zero(discriminant) == 0) {
+
+        return REAL_ROOTS_WITH_SAME_VALUES;
+
+    } else if (If_Double_Equal_Zero(a) && If_Double_Equal_Zero(b) && If_Double_Equal_Zero(c)) {
+
+        return INFINITE_NUMBER_OF_ROOTS;
+
+    } else if (If_Double_Equal_Zero(a) && If_Double_Equal_Zero(b) && !If_Double_Equal_Zero(c)) {
+
+        return NO_ROOTS;
+
+    } else if (If_Double_Equal_Zero(a)){
+
+        return LINEAR_EQUATION;
+
+    } else {
+
+        return IMAGINARY_ROOTS;
     }
 }
 
-void solve_quadratic_function(int a, int b, int c, double *x1, double *x2){
 
-    printf("a: %d, b: %d, c: %d\n", a, b, c);
+void print_solution(struct Solution solution) {
 
-    if (a == 0 && b == 0 && c == 0) {
-        printf("There is an infinite number of solutions\n");
-        return ;
-    }
+    switch (solution.type) {
 
-    if (a == 0 && b == 0) {
-        printf("There are no solutions\n");
-        return ;
-    }
+        case REAL_ROOTS_WITH_DIFFERENT_VALUES:
+            printf("Roots are real and different: \n");
+            printf("First root: %lf\n", solution.first_root);
+            printf("Second root: %lf\n", solution.second_root);
+            break;
 
-    if (a != 0) {
+        case REAL_ROOTS_WITH_SAME_VALUES:
+            printf("Roots are real and same: \n");
+            printf("Root: %lf\n", solution.first_root);
+            break;
 
-        find_quadratic_roots(a, b, c, x1, x2);
+        case IMAGINARY_ROOTS:
+            printf("Roots are complex and different \n");
+            printf("First root: %lf + %lfi\n", solution.real_part, solution.imaginary_part);
+            printf("Second root: %lf - %lfi\n", solution.real_part, solution.imaginary_part);
+            break;
 
-    } else if (a == 0) {
-        printf("There is only one root\n");
-        *x1 = -c * 1.0 / b;
+        case LINEAR_EQUATION:
+            printf("The equation is linear and has one root: \n");
+            printf("Root: %lf\n", solution.one_root);
+            break;
 
-        if (b == 1) {
-            printf("The root of the equation x + (%d) = 0 is x = %lf", c, *x1);
-        }
+        case INFINITE_NUMBER_OF_ROOTS:
+            printf("There is an infinite number of roots \n");
+            break;
 
-        printf("The root of the equation %dx + (%d) = 0 is x = %lf", b, c, *x1);
+        case NO_ROOTS:
+            printf("The equation has no roots \n");
+            break;
+
+        default:
+            break;
     }
 }
 
-void equation_input(int *a, int *b, int *c){
+struct Solution solve_quadratic_equation(double a, double b, double c) {
 
-    char input[8];
+    double discriminant = (b * b) - 4 * a * c;
+
+    struct Solution solution;
+
+    solution.type = find_root_type(discriminant, a, b, c);
+
+    switch (solution.type) {
+
+        case REAL_ROOTS_WITH_DIFFERENT_VALUES:
+            solution.first_root = (-b + sqrt(fabs(discriminant))) / (2 * a);
+            solution.second_root = (-b - sqrt(fabs(discriminant))) / (2 * a);
+            break;
+
+        case REAL_ROOTS_WITH_SAME_VALUES:
+            solution.first_root = solution.second_root = -b / (2 * a);
+            break;
+
+        case IMAGINARY_ROOTS:
+            solution.real_part = -b / (2 * a);
+            solution.imaginary_part = sqrt(fabs(discriminant)) / (2 * a);
+            break;
+
+        case LINEAR_EQUATION:
+            solution.one_root = -c / (2 * a);
+
+        default:
+            break;
+    }
+    return solution;
+}
+
+void equation_input(double *a, double *b, double *c){
+
+    char input[8] = "";
     bool a_has_input = false, b_has_input = false, c_has_input = false;
 
-    while (int(a_has_input) + int(c_has_input) + int(b_has_input) < 3) {
+    while (double(a_has_input) + double(c_has_input) + double(b_has_input) < 3) {
 
 
         if (fgets(input, sizeof input, stdin)) {
 
-            char *check;
-            int tmp = (int) strtol(input, &check, 10);
+            char *check = nullptr;
+            double tmp = (double) strtol(input, &check, 10);
 
             if (!isspace(*check) && *check != 0) {
                 printf("%s is not a valid integer: try again\n", input);
@@ -110,12 +173,11 @@ void equation_input(int *a, int *b, int *c){
 
 int main() {
 
-    int a, b, c;
+    double a = 0, b = 0, c = 0;
 
     /// the solutions of the equation
-    double x1, x2;
 
     equation_input(&a, &b, &c);
-
-    solve_quadratic_function(a, b, c, &x1, &x2);
+    struct Solution solution = solve_quadratic_equation(a, b, c);
+    print_solution(solution);
 }
